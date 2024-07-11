2bd749f8e66ece1a5c4166cccb1166691abe6a10
Andrew Sykes <andrewsykes@btinternet.com>
2024-07-11 17:31:26 +0100

    tidy

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-#"docker container stop polo-nginx-run -t 1" only use if we want to wait for it to shutdown using -t as seconds to wait
+#"docker container stop polo-nginx-run -t 1"
+#only use if we want to wait for it to shutdown using -t as seconds to wait
 docker container rm -f polo-nginx-run
 docker build -t polo-nginx . --no-cache
 docker run --name polo-nginx-run -d -p 80:80 polo-nginx
