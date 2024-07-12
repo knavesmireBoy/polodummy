89e298960c89210b286dedde1e9cd55b84649d66
Andrew Sykes <andrewsykes@btinternet.com>
2024-07-12 08:04:06 +0100

    tomorrow

--- websites/default/public/index.php
+++ websites/default/public/index.php
@@ -2,4 +2,4 @@
 $name = 'Bolt';
 $today = 'Friday';
 ?>
-<h2><?= "hello my name is $name, today is $today"; ?></h2>
+<h2><?= "hello my name is $name, do you know what today is?, its tomorrow"; ?></h2>
