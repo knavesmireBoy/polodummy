c80019857116999dedf7cc37df45f6c7df4bbea8
Andrew Sykes <andrewsykes@btinternet.com>
2024-07-12 09:11:09 +0100

    style

--- websites/default/public/index.php
+++ websites/default/public/index.php
@@ -8,8 +8,8 @@ $today = 'Friday';
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width,minimum-scale=1" />
     <title>Upload Form</title>
-    <link href="style.css" rel="stylesheet" type="text/css" />
+    <link href="resources/css/style.css" rel="stylesheet" type="text/css" />
   </head>
   <body>
-<h2><?= "hello my name is $name, dand today is $today"; ?></h2>
+<h2><?= "hello my name is $name, and today is $today"; ?></h2>
 </body>
