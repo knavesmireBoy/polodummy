e63b97dc2473c5f6a742693777071bca4e6404c5
Andrew Sykes <andrewsykes@btinternet.com>
2024-07-12 08:51:20 +0100

    html

--- websites/default/public/index.php
+++ websites/default/public/index.php
@@ -2,4 +2,14 @@
 $name = 'Bolt';
 $today = 'Friday';
 ?>
-<h2><?= "hello my name is $name, do you know what today is?, its tomorrow"; ?></h2>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width,minimum-scale=1" />
+    <title>Upload Form</title>
+    <link href="style.css" rel="stylesheet" type="text/css" />
+  </head>
+  <body>
+<h2><?= "hello my name is $name, dand today is $today"; ?></h2>
+</body>
